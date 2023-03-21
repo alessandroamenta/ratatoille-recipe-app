a8a9bdfc0fac0d238d94acc5d52ac18b90d97f74
alessandroamenta <alessandroamenta1@gmail.com>
2023-03-22 00:11:36 +0100

    second commit

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/api/recipe", async (req, res) => {
+  const { ingredients } = req.body;
+  const apiKey = process.env.SPOONACULAR_API_KEY;
+  const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients.join(
+    ","
+  )}&number=1&apiKey=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+    const recipeId = response.data[0].id;
+    const recipeUrl = `https://api.spoonacular.com/recipes/${recipeId}/analyzedInstructions?apiKey=${apiKey}`;
+
+    const instructionsResponse = await axios.get(recipeUrl);
+    console.log(instructionsResponse.data);
+
+    res.json({
+      recipe: {
+        title: response.data[0].title,
+        image: response.data[0].image,
+        instructions: instructionsResponse.data[0].steps.map(
+          (step) => step.step
+        ),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ client/src/Recipe.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "./Recipe.css";
+
+function Recipe() {
+  const [ingredients, setIngredients] = useState("");
+  const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    console.log("Ingredients:", ingredients.split(","));
+    if (ingredients.trim() === "") {
+      alert("Please enter at least one ingredient.");
+      setLoading(false);
+      return;
+    }
+    try {
+      const response = await fetch("http://localhost:5000/api/recipe", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ingredients: ingredients.split(",") }),
+      });
+      const data = await response.json();
+      if (data.recipe) {
+        console.log(data.recipe)
+        setRecipe(data.recipe);
+      } else {
+        setRecipe(null);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error(error.message);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="recipe-container">
+      <h1 className="recipe-title">What ingredients do you have?</h1>
+      <p>Super simple app to find any recipe based on random items you have around</p>
+      <form onSubmit={handleSubmit} className="recipe-form">
+        <input
+          type="text"
+          value={ingredients}
+          onChange={(e) => setIngredients(e.target.value)}
+          placeholder="Enter ingredients (separated by commas)"
+          className="recipe-input"
+        />
+        <button type="submit" className="recipe-button">
+          {loading ? "Loading..." : "Get Recipe"}
+        </button>
+      </form>
+      {recipe ? (
+        <div className="recipe-result">
+          {recipe.name && (
+            <h2 className="recipe-result-title">{recipe.name}</h2>
+          )}
+          <div className="recipe-instructions">
+            <h3 className="recipe-instructions-title">Instructions:</h3>
+            {recipe.instructions.map((step, index) => (
+              <p key={index} className="recipe-instructions-text">
+                {index + 1}. {step}
+              </p>
+            ))}
+            <div className="recipe-image">
+              <img src={recipe.image} alt="recipe" className="recipe-image" />
+            </div>
+          </div>
+        </div>
+      ) : null}
+      {loading && <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default Recipe;
+
