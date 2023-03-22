512630a80f48018fd47fe80463a882da4ff96571
alessandroamenta <alessandroamenta1@gmail.com>
2023-03-22 15:24:33 +0100

    just added the vercel url

--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -16,7 +16,7 @@ function Recipe() {
       return;
     }
     try {
-      const response = await fetch("http://localhost:8080/api/recipe", {
+      const response = await fetch("https://ratatoille-recipe-app-backend.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
